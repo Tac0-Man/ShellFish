06b5a85cae4c84adcc799de5df9dba4582354506
Arthur23730 <arthurpeeters6@gmail.com>
2026-06-08 12:25:07 +0200

    Menu
    Added in game menu

---
+++ Assets/Scripts/GoMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoMenu : MonoBehaviour
+{
+   public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitButton : MonoBehaviour
+{
+   public void Quit()
+    {
+        Application.Quit();
+    }
+}
